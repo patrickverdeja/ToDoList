58d5888426e3eb1211a53a038f766538c5f275ec
patrickverdeja <84103820+patrickverdeja@users.noreply.github.com>
2022-12-11 22:16:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+const todos = document.getElementById("todos")
+const todayDateEl = document.getElementById("todayDate")
+
+const todayIs = new Date();
+
+todayDateEl.innerHTML = todayIs.toLocaleDateString('en-US')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    addTodo();
+
+    
+});
+
+function addTodo(){
+    const todoText = input.value;
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+        })
+
+        todoEl.addEventListener('contextmenu',
+        (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+        })
+
+        todos.appendChild(todoEl)
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS(){
+    const todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+        text: todoEl.innerText,
+        completed: todoEl.classList.contains('completed')
+    })
+
+    })
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
